a9c74d4ccfc05e6a99b0c910acbdeb553577e62d
Vaishak Suresh <vaishak.suresh@gmail.com>
2013-04-27 19:00:53 -0700

    Server for REST Calls

--- MOOCClient/registeruser/views.py
+++ MOOCClient/registeruser/views.py
@@ -3,7 +3,5 @@ import json,requests
 
 def home(request):
 	r = requests.get('https://www.coursera.org/maestro/api/topic/list?full=1')
-	#r = requests.get('https://www.coursera.org/maestro/api/university/list')
-	#data = json.dumps(r.json())
 	return render_to_response('index.html',{'courses': r.json()})
 
